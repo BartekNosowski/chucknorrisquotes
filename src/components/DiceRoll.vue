a9cf67abf107ad336c7f39d5e21a07b42ee0ffa2
Bartek <bartosz.nosowski@ives-system.com>
2021-08-02 14:16:40 +0200

    added games history, localstorage set/get

--- src/components/DiceRoll.vue
+++ src/components/DiceRoll.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container bg-dark border dices">
+  <div class="container bg-dark border border-radius">
     <div class="header">The Dice Game</div>
     <button type="button" class="btn border game-rules" data-toggle="modal" data-target="#exampleModalCenter">Game
       rules
@@ -9,8 +9,8 @@
     <div class="text-styling">Roll types:</div>
     <span></span>
     <div class="row" style="justify-content: center">
-      <button @click="dicing(false)" class="button-special-lower m-1">Lower</button>
-      <button @click="dicing(true)" class="button-special-higher m-1">Higher</button>
+      <button @click="dicing(false)" class="btn button-special-lower p-0 m-1">Lower</button>
+      <button @click="dicing(true)" class="btn button-special-higher p-0 m-1">Higher</button>
     </div>
     <!--    <span v-if="dices" class="text-styling">{{ dices.dice[0].value }}</span>-->
     <img v-if="dices" v-bind:src="img"/>
@@ -22,7 +22,25 @@
         <span class="text-styling">Points earned: {{ parseFloat(howManyPoints).toFixed(1) }} / 3</span>
       </div>
     </div>
+    <div class="row text-white mx-0 px-0 my-3 start-game" style="justify-content: center">Games History</div>
+    <div>
+      <table class="table text-white">
+        <thead>
+        <tr>
+          <th scope="col">Game ID</th>
+          <th scope="col">Points Earned</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in gamesHistory" v-bind:key="item.gameId">
+          <td>{{ item.gameId }}</td>
+          <td>{{ item.pointsAtGameEnd }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
+
   <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
        aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -58,14 +76,12 @@
       </div>
     </div>
   </div>
-
-
   <div id="myModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header bg-info">
-          <h4 class="modal-title ">Reload previous game?</h4>
+          <h4 class="modal-title">Reload previous game?</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-footer" style="justify-content: center">
@@ -87,14 +103,20 @@ export default {
       dices: null,
       dice: "",
       value: 0,
-      whichRound: 0,
+      whichRound: 1,
       howManyPoints: 0,
       modalShow: false,
+      gameResult: {
+        gameId: 0,
+        pointsAtGameEnd: 0,
+      },
+      retrivedGameResult: {},
+      retrivedGameHistory: [],
+      gamesHistory: [],
 
     }
   },
   methods: {
-
     firstDice() {
       axios.get('http://roll.diceapi.com/json/d6')
           .then(res => {
@@ -104,7 +126,7 @@ export default {
           })
     },
     resetRoundPoints() {
-      this.whichRound = 0;
+      this.whichRound = 1;
       this.howManyPoints = 0;
     },
     dicing(checkGreater) {
@@ -113,22 +135,20 @@ export default {
             this.checkDice(res.data.dice[0].value, checkGreater)
             this.dices = res.data;
             this.img = "http://roll.diceapi.com/images/poorly-drawn/d6/" + res.data.dice[0].value + ".png";
+            this.finishedGames();
             this.roundChange()
+            this.saveGame();
           })
-
-
     },
     roundChange() {
       this.whichRound++;
       if (this.whichRound === 31) {
-        this.whichRound = 0
+        this.whichRound = 1
         this.howManyPoints = 0
-        return alert("Round 30 was last check your score.")
+        return alert("Round 30 was last check your score below. After clicking 'OK' scores will reset")
       }
     },
-
     checkDice(newValue, checkGreater) {
-
       if (checkGreater) {
         if (newValue > this.dices.dice[0].value) {
           this.pointsEarned()
@@ -138,31 +158,44 @@ export default {
           this.pointsEarned()
         }
       }
-
-
     },
     pointsEarned() {
-      this.howManyPoints += 0.1;
+      if (this.whichRound <= 30) {
+        this.howManyPoints += 0.1;
+      }
+      this.howManyPoints = parseFloat(parseFloat(this.howManyPoints).toFixed(2));
       if (this.howManyPoints >= 3.1) {
         this.howManyPoints = 0
         return alert("Maximum of points - congratulations!")
       }
     },
+    finishedGames() {
+      if (this.whichRound === 30) {
+        this.gameResult.gameId++;
+        this.gameResult.pointsAtGameEnd = this.howManyPoints;
+        this.gamesHistory.push({...this.gameResult})
+      }
+    },
     saveGame() {
       localStorage.whichRound = this.whichRound;
-      localStorage.howManyPoints = this.howManyPoints;
+      localStorage.howManyPoints = (this.howManyPoints).toString();
+      localStorage.setItem('gameResult', JSON.stringify(this.gameResult))
+      localStorage.gamesHistory = JSON.stringify(this.gamesHistory)
     },
-
     loadGame() {
       if (localStorage.whichRound != null && localStorage.howManyPoints != null) {
         this.whichRound = localStorage.whichRound
-        this.howManyPoints = localStorage.howManyPoints
+        this.howManyPoints = parseFloat(localStorage.howManyPoints)
       }
+      this.retrivedGameResult = JSON.parse(localStorage.getItem('gameResult'))
+      this.retrivedGameHistory = JSON.parse(localStorage.getItem('gamesHistory'))
     }
   },
   created: function () {
     this.loadGame();
     this.firstDice();
+    this.finishedGames();
+    this.saveGame();
   },
   mounted() {
     if (localStorage.whichRound) {
